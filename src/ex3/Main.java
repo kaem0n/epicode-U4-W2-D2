0463540289bed50fd88e97a8aa59828b18b93f0f
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-03-12 18:01:50 +0100

    more code polish

--- src/ex3/Main.java
+++ src/ex3/Main.java
@@ -2,8 +2,6 @@ package ex3;
 
 import ex3.entities.Contacts;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println();
@@ -19,57 +17,6 @@ public class Main {
         myContacts.addContact("Re Kaio", "345 6789110");
         myContacts.addContact("Popo", "345 6789111");
 
-        handleContacts(myContacts);
-    }
-
-    public static void handleContacts(Contacts contacts) {
-        loop: while (true) {
-            System.out.println();
-            Scanner sc = new Scanner(System.in);
-            System.out.println("CHOOSE AN OPTION: ");
-            System.out.println("1. Add a new contact");
-            System.out.println("2. Delete a contact");
-            System.out.println("3. Browse your contacts");
-            System.out.println("4. Search a contact (by name)");
-            System.out.println("5. Search a contact (by phone number)");
-            System.out.println("0. Exit");
-            String input = sc.nextLine();
-            switch (input) {
-                case "0" -> {
-                    sc.close();
-                    break loop;
-                }
-                case "1" -> {
-                    System.out.println();
-                    System.out.println("INSERT A NAME:");
-                    String name = sc.nextLine();
-                    System.out.println("INSERT A PHONE NUMBER:");
-                    String phoneNum = sc.nextLine();
-                    System.out.println();
-                    contacts.addContact(name, phoneNum);
-                }
-                case "2" -> {
-                    System.out.println();
-                    System.out.println("INSERT A NAME:");
-                    String name = sc.nextLine();
-                    System.out.println();
-                    contacts.deleteContact(name);
-                }
-                case "3" -> contacts.browseContacts();
-                case "4" -> {
-                    System.out.println();
-                    System.out.println("INSERT A NAME:");
-                    String name = sc.nextLine();
-                    contacts.searchContactByName(name);
-                }
-                case "5" -> {
-                    System.out.println();
-                    System.out.println("INSERT A PHONE NUMBER: ");
-                    String phoneNum = sc.nextLine();
-                    contacts.searchContactByPhoneNum(phoneNum);
-                }
-                default -> System.err.println("Invalid input. Try again.");
-            }
-        }
+        Contacts.handleContacts(myContacts);
     }
 }
--- src/ex3/entities/Contacts.java
+++ src/ex3/entities/Contacts.java
@@ -2,6 +2,7 @@ package ex3.entities;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Contacts {
     private final Map<String, String> contactList = new HashMap<>();
@@ -37,5 +38,56 @@ public class Contacts {
         System.out.println();
         System.out.println("List of contacts:");
         for (String name : contactList.keySet()) System.out.println(name + ": " + contactList.get(name));
+    }
+
+    public static void handleContacts(Contacts contacts) {
+        loop: while (true) {
+            System.out.println();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("CHOOSE AN OPTION: ");
+            System.out.println("1. Add a new contact");
+            System.out.println("2. Delete a contact");
+            System.out.println("3. Browse your contacts");
+            System.out.println("4. Search a contact (by name)");
+            System.out.println("5. Search a contact (by phone number)");
+            System.out.println("0. Exit");
+            String input = sc.nextLine();
+            switch (input) {
+                case "0" -> {
+                    sc.close();
+                    break loop;
+                }
+                case "1" -> {
+                    System.out.println();
+                    System.out.println("INSERT A NAME:");
+                    String name = sc.nextLine();
+                    System.out.println("INSERT A PHONE NUMBER:");
+                    String phoneNum = sc.nextLine();
+                    System.out.println();
+                    contacts.addContact(name, phoneNum);
+                }
+                case "2" -> {
+                    System.out.println();
+                    System.out.println("INSERT A NAME:");
+                    String name = sc.nextLine();
+                    System.out.println();
+                    contacts.deleteContact(name);
+                }
+                case "3" -> contacts.browseContacts();
+                case "4" -> {
+                    System.out.println();
+                    System.out.println("INSERT A NAME:");
+                    String name = sc.nextLine();
+                    contacts.searchContactByName(name);
+                }
+                case "5" -> {
+                    System.out.println();
+                    System.out.println("INSERT A PHONE NUMBER: ");
+                    String phoneNum = sc.nextLine();
+                    contacts.searchContactByPhoneNum(phoneNum);
+                }
+                default -> System.err.println("Invalid input. Try again.");
+            }
+        }
     }
 }
